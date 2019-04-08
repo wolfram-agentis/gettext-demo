332b27102b907eaf716db098c5319a25c7256fdc
Wolfram Rong <wolfram@agentisenergy.com>
2019-04-08 15:46:57 -0500

    add a missing condition

--- locale.php
+++ locale.php
@@ -32,7 +32,9 @@ function getLanguage($validLanguages) {
     // allow hard overwrite via ?lang=x param
     if (isset($_GET['lang']) && in_array($_GET['lang'], $validLanguages)) {
         return $_GET['lang'];
-    } elseif (!isset($_SESSION['lang'])) {
+    } elseif (isset($_SESSION['lang'])) {
+        return $_SESSION['lang'];
+    } else {
         $acceptedLanguages = parseHttpAcceptLanguage();
 
         for ($i = 0; $i < count($acceptedLanguages); $i++) {
